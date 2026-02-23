79ae916954f1fc12c1df0a106c19625b498542b7
sparkyjolteon <neerajbalamurugan@gmail.com>
2026-02-23 14:50:08 +0530

    [Your_name]Added [usecasename]

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,7 @@
+public class PalindromeCheckerApp {
+    public static void main(String[] args){
+        System.out.println("Welcome to Palindrome Checker Management System\n" +
+                "Version : 1.0\n" +
+                "System initalized successfully\n");
+    }
+}
